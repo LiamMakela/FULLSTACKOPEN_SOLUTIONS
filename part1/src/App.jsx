a554e15637ca7ee447b9ef805cd96f271b79dfd3
Liam Makela <liammakela06@gmail.com>
2025-11-22 16:10:16 -0600

    completed!

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,28 +1,69 @@
-const Hello = (props) => {
+import { useState } from 'react'
 
-  console.log(props)
-  return (
+const Button = (props) => {
+  return(
+    <button onClick = {props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Display = (props) => {
+  return(
     <div>
-      <p>
-
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      {props.text}
     </div>
   )
 }
 
-const App = () => {
 
-  const name = 'Peter'
-  const age = 10
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Uint8Array(8))
+  const [mostVotes, setMostVotes] = useState(0)
+
+  const nextAnecdote = () => {
+    if (selected >= anecdotes.length - 1){
+      setSelected(0)
+    } else {
+      setSelected(selected + 1)
+    }
+  }
+
+  const newVote = () => {
+    console.log(selected)
+    console.log(votes)
+    console.log(votes[selected])
+
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes( copy)
+    if (copy[selected] > copy[mostVotes]) {
+      setMostVotes(selected)
+    }
+  }
 
   return (
-    <>
-      <h1>Greetings</h1>
-
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
-    </>
+    <div>
+      <h1>Anecdote of the day</h1>
+      <Display text = {anecdotes[selected]} />
+      <Button onClick = {nextAnecdote} text ="next anecdote"/>
+      <Display text = {"has " + votes[selected] + " votes"} />
+      <Button onClick = {newVote} text ="vote"/>
+      <h1>Anecdote with most votes</h1>
+      <Display text = {anecdotes[mostVotes]} />
+    </div>
   )
 }
 
